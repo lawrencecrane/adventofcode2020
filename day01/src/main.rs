a0e866fa6711c14146ad818135dbd237609b2c00
Lauri Kurki <lauri.kurki@siili.com>
2020-12-01 18:06:14 +0200

    Create first working version of finding three numbers
    Passes tests...

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -6,16 +6,17 @@ use std::path::Path;
 fn main() {
     let lines = read_lines("./data/input").unwrap();
 
-    let (a, b) = find_matching_sum(lines, &to_number, 2020).unwrap();
+    let (a, b) = find_matching_sum(lines, &to_number, 2020, &_find_matching_sum).unwrap();
 
     println!("{} and {}, answer: {}", a, b, a * b);
 }
 
-fn find_matching_sum<I>(
+fn find_matching_sum<I, T>(
     numbers: I,
     to_number: &dyn Fn(<I as Iterator>::Item) -> u32,
     sum: u32,
-) -> Option<(u32, u32)>
+    finder: &dyn Fn(&Vec<u32>, &u32, u32) -> Option<T>,
+) -> Option<T>
 where
     I: Iterator,
 {
@@ -26,7 +27,7 @@ where
                 Some(_) => (numbers, previous_sum),
                 None => {
                     let n = to_number(x);
-                    let sum = _find_matching_sum(&numbers, &n, sum);
+                    let sum = finder(&numbers, &n, sum);
                     numbers.push(n);
 
                     (numbers, sum)
@@ -35,6 +36,26 @@ where
         );
 
     found_sum
+}
+
+fn _find_matching_sum_of_three(xs: &Vec<u32>, n: &u32, sum: u32) -> Option<(u32, u32, u32)> {
+    let (_, matching) =
+        xs.iter().fold(
+            (Vec::new(), None),
+            |(mut numbers, previous_sum), x| match previous_sum {
+                Some(_) => (numbers, previous_sum),
+                None => {
+                    let matching = _find_matching_sum(&numbers, &(x + n), sum)
+                        .and_then(|(a, _)| Some((a, *x, *n)));
+
+                    numbers.push(*x);
+
+                    (numbers, matching)
+                }
+            },
+        );
+
+    matching
 }
 
 fn _find_matching_sum(xs: &Vec<u32>, n: &u32, sum: u32) -> Option<(u32, u32)> {
@@ -65,8 +86,25 @@ mod tests {
     fn find_matching_sum_test() {
         let numbers: Vec<u32> = Vec::from([1721, 979, 366, 299, 675, 1456]);
 
-        let (a, b) = super::find_matching_sum(numbers.iter(), &to_number, 2020).unwrap();
+        let (a, b) =
+            super::find_matching_sum(numbers.iter(), &to_number, 2020, &super::_find_matching_sum)
+                .unwrap();
 
         assert_eq!(a * b, 514579);
+    }
+
+    #[test]
+    fn find_matching_sum_of_three_test() {
+        let numbers: Vec<u32> = Vec::from([1721, 979, 366, 299, 675, 1456]);
+
+        let (a, b, c) = super::find_matching_sum(
+            numbers.iter(),
+            &to_number,
+            2020,
+            &super::_find_matching_sum_of_three,
+        )
+        .unwrap();
+
+        assert_eq!(a * b * c, 241861950);
     }
 }
