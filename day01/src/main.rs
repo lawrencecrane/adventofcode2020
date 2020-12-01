bb7162a1cbf3784e590327c5f3dc4f9bc5a26436
Lauri Kurki <lauri.kurki@siili.com>
2020-12-01 19:26:34 +0200

    Dont clone in find_sum_of_three and read input file only once

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -4,40 +4,37 @@ use std::iter::Iterator;
 use std::path::Path;
 
 fn main() {
-    solve_part_one();
-    solve_part_two();
+    let numbers: Vec<u32> = read_lines("./data/input").unwrap().map(to_number).collect();
+
+    solve_part_one(&numbers);
+    solve_part_two(&numbers);
 }
 
-fn solve_part_one() {
-    let lines = read_lines("./data/input").unwrap();
-
-    let (a, b) = find_matching_sum(lines.map(to_number), 2020, &_find_matching_sum).unwrap();
+fn solve_part_one(numbers: &Vec<u32>) {
+    let (a, b) = find_matching_sum(numbers.iter(), 2020, &_find_matching_sum).unwrap();
 
     println!("PART 1: {} and {}, answer: {}", a, b, a * b);
 }
 
-fn solve_part_two() {
-    let lines = read_lines("./data/input").unwrap();
-
-    let (a, b, c) =
-        find_matching_sum(lines.map(to_number), 2020, &_find_matching_sum_of_three).unwrap();
+fn solve_part_two(numbers: &Vec<u32>) {
+    let (a, b, c) = find_matching_sum(numbers.iter(), 2020, &_find_matching_sum_of_three).unwrap();
 
     println!("PART 2: {}, {} and {}, answer: {}", a, b, c, a * b * c);
 }
 
-fn find_matching_sum<I, T>(
+fn find_matching_sum<'a, I, T>(
     numbers: I,
     sum: u32,
     finder: &dyn Fn(&Vec<u32>, &u32, u32) -> Option<T>,
 ) -> Option<T>
 where
-    I: Iterator<Item = u32>,
+    I: Iterator<Item = &'a u32>,
 {
     let (_, matching) = numbers.fold((Vec::new(), None), |(mut xs, matching), x| match matching {
         Some(_) => (xs, matching),
         None => {
             let sum = finder(&xs, &x, sum);
-            xs.push(x);
+            xs.push(*x);
 
             (xs, sum)
         }
@@ -51,7 +48,7 @@ fn _find_matching_sum_of_three(
     number: &u32,
     sum: u32,
 ) -> Option<(u32, u32, u32)> {
-    find_matching_sum(numbers.clone().into_iter(), sum, &|xs, x, _| {
+    find_matching_sum(numbers.iter(), sum, &|xs, x, _| {
         _find_matching_sum(xs, &(number + x), sum).and_then(|(a, _)| Some((a, *x, *number)))
     })
 }
@@ -81,8 +78,7 @@ mod tests {
         let numbers: Vec<u32> = Vec::from([1721, 979, 366, 299, 675, 1456]);
 
         let (a, b) =
-            super::find_matching_sum(numbers.into_iter(), 2020, &super::_find_matching_sum)
-                .unwrap();
+            super::find_matching_sum(numbers.iter(), 2020, &super::_find_matching_sum).unwrap();
 
         assert_eq!(a * b, 514579);
     }
@@ -91,12 +87,9 @@ mod tests {
     fn find_matching_sum_of_three_test() {
         let numbers: Vec<u32> = Vec::from([1721, 979, 366, 299, 675, 1456]);
 
-        let (a, b, c) = super::find_matching_sum(
-            numbers.into_iter(),
-            2020,
-            &super::_find_matching_sum_of_three,
-        )
-        .unwrap();
+        let (a, b, c) =
+            super::find_matching_sum(numbers.iter(), 2020, &super::_find_matching_sum_of_three)
+                .unwrap();
 
         assert_eq!(a * b * c, 241861950);
     }
