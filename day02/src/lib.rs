1e4839f3275d01fb82a70538de9c40430bd3eda3
Lauri Kurki <lauri.kurki@siili.com>
2020-12-02 17:40:01 +0200

    Add unit test for n_valid_passwords

--- day02/src/lib.rs
+++ day02/src/lib.rs
@@ -1,6 +1,13 @@
+pub fn n_valid_passwords(passwords: &Vec<&str>) -> usize {
+    passwords.len()
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn find_valid_passwords_test() {
+        let passwords = vec!["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"];
+
+        assert_eq!(super::n_valid_passwords(&passwords), 2)
     }
 }
