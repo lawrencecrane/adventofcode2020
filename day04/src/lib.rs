37352fd1f83a236d11ce841c7fdae536adc5a28f
Lauri Kurki <lauri.kurki@siili.com>
2020-12-04 19:01:42 +0200

    Use hashmap to keep passport data, create test for n_valid

--- day04/src/lib.rs
+++ day04/src/lib.rs
@@ -1,6 +1,11 @@
+use std::collections::HashMap;
 use std::iter::Iterator;
 
-pub fn to_passports<I>(passports: I) -> Vec<Vec<String>>
+pub fn n_valid(passports: &Vec<HashMap<String, String>>) -> usize {
+    0
+}
+
+pub fn to_passports<I>(passports: I) -> Vec<HashMap<String, String>>
 where
     I: Iterator<Item = String>,
 {
@@ -28,11 +33,52 @@ where
         },
     );
 
-    passports
+    passports.iter().map(to_passport).collect()
+}
+
+fn to_passport(passport: &Vec<String>) -> HashMap<String, String> {
+    passport.iter().fold(HashMap::new(), |mut map, field| {
+        let mut data = field.split(':');
+
+        match (data.next(), data.next()) {
+            (Some(key), Some(value)) => {
+                map.insert(key.to_string(), value.to_string());
+            }
+            _ => {}
+        };
+
+        map
+    })
 }
 
 #[cfg(test)]
 mod tests {
+    fn create_factory() -> Vec<String> {
+        vec![
+            "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd",
+            "byr:1937 iyr:2017 cid:147 hgt:183cm",
+            "",
+            "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884",
+            "hcl:#cfa07d byr:1929",
+            "",
+            "hcl:#ae17e1 iyr:2013",
+            "eyr:2024",
+            "ecl:brn pid:760753108 byr:1931",
+            "hgt:179cm",
+            "",
+            "hcl:#cfa07d eyr:2025 pid:166559648",
+            "iyr:2011 ecl:brn hgt:59in",
+            "",
+        ]
+        .iter()
+        .map(|s| s.to_string())
+        .collect()
+    }
+
     #[test]
-    fn test_() {}
+    fn test_n_valid() {
+        let passports = super::to_passports(create_factory().into_iter());
+
+        assert_eq!(super::n_valid(&passports), 2);
+    }
 }
