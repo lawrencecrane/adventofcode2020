c81dc71f7adc2a6e8363955bcb9c0bc8022067e9
Lauri Kurki <lauri.kurki@siili.com>
2020-12-07 16:53:40 +0200

    Implement bag parser

--- day07/src/lib.rs
+++ day07/src/lib.rs
@@ -2,6 +2,61 @@ pub fn n_bags_that_can_contain(bags: &Vec<String>, bag: String) -> usize {
     0
 }
 
+pub fn to_bags(bags: &Vec<String>) -> Vec<Bag> {
+    bags.iter().map(&to_bag).collect()
+}
+
+fn to_bag(bag: &String) -> Bag {
+    let mut s = bag.split("contain");
+
+    let name = s
+        .next()
+        .unwrap()
+        .split("bag")
+        .next()
+        .unwrap()
+        .trim()
+        .to_string();
+
+    let contains: Vec<Container> = s
+        .next()
+        .unwrap()
+        .split(',')
+        .map(|x| {
+            let (n, name) =
+                split_once(x.split("bag").next().unwrap().trim().to_string(), " ").unwrap();
+
+            Container {
+                name,
+                n: n.parse().unwrap(),
+            }
+        })
+        .collect();
+
+    Bag { name, contains }
+}
+
+fn split_once(x: String, pattern: &str) -> Option<(String, String)> {
+    let mut s = x.splitn(2, pattern);
+
+    match (s.next(), s.next()) {
+        (Some(fst), Some(snd)) => Some((fst.to_string(), snd.to_string())),
+        _ => None,
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Bag {
+    name: String,
+    contains: Vec<Container>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Container {
+    name: String,
+    n: usize,
+}
+
 #[cfg(test)]
 mod tests {
     fn create_factory() -> Vec<String> {
@@ -28,6 +83,28 @@ mod tests {
         assert_eq!(
             super::n_bags_that_can_contain(&bags, "shiny gold".to_string()),
             4
+        );
+    }
+
+    #[test]
+    fn test_to_bag() {
+        assert_eq!(
+            super::to_bag(
+                &"light red bags contain 1 bright white bag, 2 muted yellow bags.".to_string()
+            ),
+            super::Bag {
+                name: "light red".to_string(),
+                contains: vec![
+                    super::Container {
+                        name: "bright white".to_string(),
+                        n: 1
+                    },
+                    super::Container {
+                        name: "muted yellow".to_string(),
+                        n: 2,
+                    }
+                ]
+            }
         );
     }
 }
