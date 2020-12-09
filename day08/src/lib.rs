dd03a830bb4833acfcb6473bba16417a3feaecde
Lauri Kurki <lauri.kurki@siili.com>
2020-12-09 16:16:51 +0200

    Implement execute and solve part 1 for day 8

---
+++ day08/src/lib.rs
@@ -0,0 +1,91 @@
+pub fn execute(code: Vec<Code>) -> isize {
+    _execute(code, 0, 0, Vec::new())
+}
+
+fn _execute(code: Vec<Code>, acc: isize, index: usize, mut executed: Vec<usize>) -> isize {
+    match executed.contains(&index) {
+        true => acc,
+        false => {
+            executed.push(index);
+
+            let instruction = code[index];
+
+            match instruction.instruction {
+                Instruction::NOP => _execute(code, acc, index + 1, executed),
+                Instruction::ACC => _execute(code, acc + instruction.value, index + 1, executed),
+                Instruction::JMP => _execute(
+                    code,
+                    acc,
+                    (index as isize + instruction.value) as usize,
+                    executed,
+                ),
+            }
+        }
+    }
+}
+
+pub fn to_codes<I>(raw: I) -> Vec<Code>
+where
+    I: Iterator<Item = String>,
+{
+    raw.map(|x| to_code(x).unwrap()).collect()
+}
+
+fn to_code(x: String) -> Option<Code> {
+    let mut s = x.split_whitespace();
+
+    match (s.next(), s.next()) {
+        (Some(instruction), Some(value)) => Some(Code {
+            instruction: Instruction::from_string(instruction).unwrap(),
+            value: value.parse().unwrap(),
+        }),
+        _ => None,
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct Code {
+    instruction: Instruction,
+    value: isize,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Instruction {
+    NOP,
+    ACC,
+    JMP,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct ParseInstructionError {}
+
+impl Instruction {
+    fn from_string(input: &str) -> Result<Instruction, ParseInstructionError> {
+        match input {
+            "nop" => Ok(Instruction::NOP),
+            "acc" => Ok(Instruction::ACC),
+            "jmp" => Ok(Instruction::JMP),
+            _ => Err(ParseInstructionError {}),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    fn create_factory() -> Vec<String> {
+        vec![
+            "nop +0", "acc +1", "jmp +4", "acc +3", "jmp -3", "acc -99", "acc +1", "jmp -4",
+            "acc +6",
+        ]
+        .iter()
+        .map(|x| x.to_string())
+        .collect()
+    }
+
+    #[test]
+    fn test_execute() {
+        let code = super::to_codes(create_factory().into_iter());
+
+        assert_eq!(super::execute(code), 5)
+    }
+}
---
+++ day08/src/main.rs
@@ -0,0 +1,11 @@
+use aoc_utils::*;
+
+mod lib;
+
+use lib::*;
+
+fn main() {
+    let code = to_codes(read_lines("../data/08_input").unwrap().map(|s| s.unwrap()));
+
+    println!("PART 1 | {}", execute(code))
+}
