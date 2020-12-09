83041fabc15c5d9f7e3a8307590eca9c0536e2bd
Lauri Kurki <lauri.kurki@siili.com>
2020-12-09 17:25:03 +0200

    Create skeleton for find_first_mismatch

--- day09/src/lib.rs
+++ day09/src/lib.rs
@@ -1,9 +1,20 @@
+pub fn find_first_mismatch(numbers: Vec<usize>, preamble: usize) -> usize {
+    0
+}
+
 #[cfg(test)]
 mod tests {
-    fn create_factory() -> Vec<String> {
-        vec![""].iter().map(|x| x.to_string()).collect()
+    fn create_factory() -> Vec<usize> {
+        vec![
+            35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309,
+            576,
+        ]
     }
 
     #[test]
-    fn test_() {}
+    fn test_find_first_mismatch() {
+        let numbers = create_factory();
+
+        assert_eq!(super::find_first_mismatch(numbers, 5), 127);
+    }
 }
--- day09/src/main.rs
+++ day09/src/main.rs
@@ -5,5 +5,10 @@ mod lib;
 use lib::*;
 
 fn main() {
-    let data = read_lines("../data/09_input").unwrap().map(|s| s.unwrap());
+    let data: Vec<usize> = read_lines("../data/09_input")
+        .unwrap()
+        .map(|s| s.unwrap().parse().unwrap())
+        .collect();
+
+    println!("PART 1 | {}", find_first_mismatch(data, 25));
 }
