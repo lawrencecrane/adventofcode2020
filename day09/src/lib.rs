bc9c31a11ad3a0078ae4db0f2b4ff029c435a438
Lauri Kurki <lauri.kurki@siili.com>
2020-12-09 17:18:33 +0200

    Initialize day 9

---
+++ day09/src/lib.rs
@@ -0,0 +1,9 @@
+#[cfg(test)]
+mod tests {
+    fn create_factory() -> Vec<String> {
+        vec![""].iter().map(|x| x.to_string()).collect()
+    }
+
+    #[test]
+    fn test_() {}
+}
---
+++ day09/src/main.rs
@@ -0,0 +1,9 @@
+use aoc_utils::*;
+
+mod lib;
+
+use lib::*;
+
+fn main() {
+    let data = read_lines("../data/09_input").unwrap().map(|s| s.unwrap());
+}
