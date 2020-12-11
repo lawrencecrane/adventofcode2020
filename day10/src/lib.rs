10d31dd0a4a20b44b90b9c5af720e4f356ffdc38
Lauri Kurki <lauri.kurki@siili.com>
2020-12-11 21:49:46 +0200

    Solve day 10 part 2 using tribonacci sequences

--- day10/src/lib.rs
+++ day10/src/lib.rs
@@ -1,19 +1,20 @@
+use aoc_utils::tribonacci::Tribonacci;
+
 // Expects the charging outlet and device's built-in adapter to be part of adapters
-// TODO: Okay this seems to be the bruteforce way, with memoization it could work,
-// but I found a hint that the optimal solution has to do with tribonacci sequence, hmm...
 pub fn n_arrangements(adapters: &Vec<usize>) -> usize {
-    let mut i = 0;
-    let mut arrangements = find_arrangements(adapters, &vec![]).unwrap();
+    let (arrangements, _) = adapters
+        .iter()
+        .fold((1, 1), |(arrangements, nrun), adapter| {
+            match adapters.contains(&(adapter + 1)) {
+                true => (arrangements, nrun + 1),
+                false => (
+                    arrangements * Tribonacci {}.into_iter().take(nrun + 1).last().unwrap(),
+                    1,
+                ),
+            }
+        });
 
-    while i < arrangements.len() {
-        if let Some(mut xs) = find_arrangements(adapters, &arrangements[i]) {
-            arrangements.append(&mut xs);
-        }
-
-        i += 1;
-    }
-
-    arrangements.len() + 1
+    arrangements
 }
 
 // Expects the charging outlet and device's built-in adapter to be part of adapters
@@ -29,46 +30,6 @@ pub fn count_jolt_differences(adapters: &Vec<usize>) -> [usize; 3] {
             });
 
     counts
-}
-
-fn find_arrangements(arrangement: &Vec<usize>, included: &Vec<usize>) -> Option<Vec<Vec<usize>>> {
-    let mut found = Vec::new();
-    let mut arrangements = Vec::new();
-
-    while let Some(index) = find_arrangement(
-        arrangement,
-        *found.last().unwrap_or(included.last().unwrap_or(&0)),
-        &included,
-    ) {
-        let mut indeces = included.clone();
-        indeces.push(index);
-
-        arrangements.push(indeces);
-        found.push(index);
-    }
-
-    match found.len() {
-        0 => None,
-        _ => Some(arrangements),
-    }
-}
-
-fn find_arrangement(arrangement: &Vec<usize>, from: usize, ignored: &Vec<usize>) -> Option<usize> {
-    arrangement
-        .iter()
-        .enumerate()
-        .skip(2 + from)
-        .find_map(|(i, adapter)| {
-            let mut prevs = (0..i).rev().filter(|x| !ignored.contains(x));
-
-            match (prevs.next(), prevs.next()) {
-                (Some(prev), Some(prevprev)) => match *adapter - arrangement[prevprev] <= 3 {
-                    true => Some(prev),
-                    false => None,
-                },
-                _ => None,
-            }
-        })
 }
 
 // Add the charging outlet and device's built-in adapter to data and sorts it
@@ -116,31 +77,5 @@ mod tests {
     fn test_n_arrangements() {
         assert_eq!(super::n_arrangements(&create_small_factory()), 8);
         assert_eq!(super::n_arrangements(&create_big_factory()), 19208);
-    }
-
-    #[test]
-    fn test_find_arrangement() {
-        let data = vec![0, 1, 2, 3, 4, 7];
-
-        assert_eq!(super::find_arrangement(&data, 0, &vec![]), Some(1));
-        assert_eq!(super::find_arrangement(&data, 1, &vec![1]), Some(2));
-        assert_eq!(super::find_arrangement(&data, 2, &vec![1, 2]), None);
-    }
-
-    #[test]
-    fn test_find_arrangements() {
-        let data = vec![0, 1, 2, 3, 4, 7];
-
-        assert_eq!(
-            super::find_arrangements(&data, &vec![]),
-            Some(vec![vec![1], vec![2], vec![3]])
-        );
-
-        assert_eq!(
-            super::find_arrangements(&data, &vec![1]),
-            Some(vec![vec![1, 2], vec![1, 3]])
-        );
-
-        assert_eq!(super::find_arrangements(&data, &vec![1, 2]), None);
     }
 }
