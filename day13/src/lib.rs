1e7c0df2888c3790749a0f1f5ee32aa70ccf86e3
Lauri Kurki <lauri.kurki@siili.com>
2020-12-13 20:10:53 +0200

    Create boilerplate for day 13

---
+++ day13/src/lib.rs
@@ -0,0 +1,39 @@
+pub fn find_earliest(timetable: &Timetable) -> (usize, usize) {
+    (0, 0)
+}
+
+pub fn parse(data: Vec<String>) -> Timetable {
+    Timetable {
+        earliest_depart_time: data[0].parse().unwrap(),
+        bus_ids: data[1]
+            .split(',')
+            .filter(|x| x != &"x")
+            .map(|x| x.parse::<usize>().unwrap())
+            .collect(),
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Timetable {
+    earliest_depart_time: usize,
+    bus_ids: Vec<usize>,
+}
+
+#[cfg(test)]
+mod tests {
+    fn create_factory() -> super::Timetable {
+        super::parse(
+            vec!["939", "7,13,x,x,59,x,31,19"]
+                .iter()
+                .map(|s| s.to_string())
+                .collect(),
+        )
+    }
+
+    #[test]
+    fn test_() {
+        let data = create_factory();
+
+        assert_eq!(super::find_earliest(&data), (944, 59));
+    }
+}
---
+++ day13/src/main.rs
@@ -0,0 +1,16 @@
+use aoc_utils::*;
+
+mod lib;
+
+use lib::*;
+
+fn main() {
+    let timetable = parse(
+        read_lines("../data/13_input")
+            .unwrap()
+            .map(|s| s.unwrap().parse().unwrap())
+            .collect(),
+    );
+
+    println!("{:?}", find_earliest(&timetable));
+}
