8919a02bad37f7c8faa1741f5cf6341b2bdddbe6
Lauri Kurki <lauri.kurki@siili.com>
2020-12-15 16:49:54 +0200

    Initialize history

--- day15/src/lib.rs
+++ day15/src/lib.rs
@@ -1,5 +1,19 @@
-pub fn play_memory_game(starting: Vec<usize>, until: usize) -> usize {
+use std::collections::HashMap;
+
+pub fn play_memory_game(starting: &Vec<usize>, until: usize) -> usize {
+    let mut history = initialize_history(starting);
+
     0
+}
+
+fn initialize_history(starting: &Vec<usize>) -> HashMap<usize, (Option<usize>, Option<usize>)> {
+    let mut history: HashMap<usize, (Option<usize>, Option<usize>)> = HashMap::new();
+
+    starting.iter().enumerate().for_each(|(i, x)| {
+        history.insert(*x, (Some(i), None));
+    });
+
+    history
 }
 
 #[cfg(test)]
@@ -7,12 +21,12 @@ mod tests {
 
     #[test]
     fn test_play_memory_game() {
-        assert_eq!(super::play_memory_game(vec![0, 3, 6], 2020), 436);
-        assert_eq!(super::play_memory_game(vec![1, 3, 2], 2020), 1);
-        assert_eq!(super::play_memory_game(vec![2, 1, 3], 2020), 10);
-        assert_eq!(super::play_memory_game(vec![1, 2, 3], 2020), 27);
-        assert_eq!(super::play_memory_game(vec![2, 3, 1], 2020), 78);
-        assert_eq!(super::play_memory_game(vec![3, 2, 1], 2020), 438);
-        assert_eq!(super::play_memory_game(vec![3, 1, 2], 2020), 1836);
+        assert_eq!(super::play_memory_game(&vec![0, 3, 6], 2020), 436);
+        assert_eq!(super::play_memory_game(&vec![1, 3, 2], 2020), 1);
+        assert_eq!(super::play_memory_game(&vec![2, 1, 3], 2020), 10);
+        assert_eq!(super::play_memory_game(&vec![1, 2, 3], 2020), 27);
+        assert_eq!(super::play_memory_game(&vec![2, 3, 1], 2020), 78);
+        assert_eq!(super::play_memory_game(&vec![3, 2, 1], 2020), 438);
+        assert_eq!(super::play_memory_game(&vec![3, 1, 2], 2020), 1836);
     }
 }
--- day15/src/main.rs
+++ day15/src/main.rs
@@ -5,6 +5,6 @@ use lib::*;
 fn main() {
     println!(
         "PART 1 | {}",
-        play_memory_game(vec![6, 13, 1, 15, 2, 0], 2020)
+        play_memory_game(&vec![6, 13, 1, 15, 2, 0], 2020)
     );
 }
