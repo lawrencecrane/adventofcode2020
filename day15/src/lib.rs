b5c59752b119b47e0d1c4169ea2f0fcbac92d2da
Lauri Kurki <lauri.kurki@siili.com>
2020-12-15 17:04:32 +0200

    Add nturn and last to MemoryGame

--- day15/src/lib.rs
+++ day15/src/lib.rs
@@ -18,24 +18,34 @@ impl Iterator for MemoryGame {
 
 impl MemoryGame {
     fn new(starting: &Vec<usize>) -> Self {
+        let (history, last) = MemoryGame::initialize_history(starting);
+
         Self {
-            history: MemoryGame::initialize_history(starting),
+            history,
+            last,
+            nturn: starting.len(),
         }
     }
 
-    fn initialize_history(starting: &Vec<usize>) -> History {
+    fn initialize_history(starting: &Vec<usize>) -> (History, usize) {
         let mut history: History = HashMap::new();
 
-        starting.iter().enumerate().for_each(|(i, x)| {
-            history.insert(*x, (Some(i), None));
-        });
+        starting
+            .iter()
+            .take(starting.len() - 1)
+            .enumerate()
+            .for_each(|(i, x)| {
+                history.insert(*x, (Some(i + 1), None));
+            });
 
-        history
+        (history, *starting.last().unwrap())
     }
 }
 
 struct MemoryGame {
     history: History,
+    nturn: usize,
+    last: usize,
 }
 
 type History = HashMap<usize, (Option<usize>, Option<usize>)>;
