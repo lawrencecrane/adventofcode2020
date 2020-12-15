8c869f899b8b4b2da804dc2aa19520ed01cada9a
Lauri Kurki <lauri.kurki@siili.com>
2020-12-15 17:45:50 +0200

    Implement memory game

--- day15/src/lib.rs
+++ day15/src/lib.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 pub fn play_memory_game(starting: &Vec<usize>, until: usize) -> usize {
     MemoryGame::new(starting)
         .into_iter()
-        .take(until)
+        .take(until - starting.len())
         .last()
         .unwrap_or(0)
 }
@@ -12,7 +12,17 @@ impl Iterator for MemoryGame {
     type Item = usize;
 
     fn next(&mut self) -> Option<usize> {
-        None
+        self.nturn += 1;
+
+        self.last = match self.history.get(&self.last) {
+            Some((Some(_), snd)) if snd.is_none() => 0,
+            Some((Some(fst), Some(snd))) => *snd - *fst,
+            _ => panic!(""),
+        };
+
+        self.insert_to_history(self.nturn, self.last);
+
+        Some(self.last)
     }
 }
 
@@ -30,15 +40,31 @@ impl MemoryGame {
     fn initialize_history(starting: &Vec<usize>) -> (History, usize) {
         let mut history: History = HashMap::new();
 
-        starting
-            .iter()
-            .take(starting.len() - 1)
-            .enumerate()
-            .for_each(|(i, x)| {
-                history.insert(*x, (Some(i + 1), None));
-            });
+        starting.iter().enumerate().for_each(|(i, x)| {
+            MemoryGame::_insert_to_history(&mut history, i + 1, *x);
+        });
 
         (history, *starting.last().unwrap())
+    }
+
+    fn insert_to_history(&mut self, nturn: usize, value: usize) {
+        MemoryGame::_insert_to_history(&mut self.history, nturn, value);
+    }
+
+    fn _insert_to_history(history: &mut History, nturn: usize, value: usize) {
+        match history.get_mut(&value) {
+            None => {
+                history.insert(value, (Some(nturn), None));
+            }
+            Some((Some(_), snd)) if snd.is_none() => {
+                *snd = Some(nturn);
+            }
+            Some((Some(fst), Some(snd))) => {
+                *fst = *snd;
+                *snd = nturn;
+            }
+            _ => (),
+        };
     }
 }
 
